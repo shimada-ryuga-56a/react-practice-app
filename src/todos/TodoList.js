5e165bb8d090adf867459ab1c1620ebf5adfca18
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-14 12:47:07 +0900

    fix: 受け渡す値の変更

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,12 +1,12 @@
 import TodoItem from './TodoItem.js';
 
-function TodoList ({ todos, onDelete, onStartEdit, editingIndex, editingText, handleEditingChange, handleUpdate }) {
+function TodoList ({ todos, onDelete, onStartEdit, editingIndex, editingText, onEditingChange, onUpdate }) {
   return (
     <div className="todo_list">
       <h2>Todoリスト</h2>
       <ul>
         {todos.map((todo, index) => (
-          <TodoItem key={index} todo={todo} onDelete={onDelete} onStartEdit={onStartEdit} editingIndex={editingIndex} editingText={editingText} handleEditingChange={handleEditingChange} handleUpdate={handleUpdate} />
+          <TodoItem key={index} index={index} todo={todo} onDelete={onDelete} onStartEdit={onStartEdit} editingIndex={editingIndex} editingText={editingText} onEditingChange={onEditingChange} onUpdate={onUpdate} />
         ))}
       </ul>
     </div>
